291e544aa962161b872739d820f7633625c653d3
Mesut <mesbol6647@gmail.com>
2024-06-03 12:45:03 +0300

    routing

--- src/routes/car.js
+++ src/routes/car.js
@@ -5,4 +5,21 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/car:
-module.exports=router
+
+const car = require('../controllers/car')
+
+// URL: /cars
+
+
+router.route('/')
+    .get(car.list)
+    .post(car.create)
+
+router.route('/:id')
+    .get(car.read)
+    .put(car.update)
+    .patch(car.update)
+    .delete(car.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router

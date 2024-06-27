3b0d9c247613d0dcb1248f7e7b18b4d0dc1c8127
Mesut <mesbol6647@gmail.com>
2024-06-27 16:02:59 +0300

    fs

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -7,13 +7,20 @@
 
 // Multer: UploadFile:
 // https://expressjs.com/en/resources/middleware/multer.html
-const multer = require('multer')
+const multer = require('multer');
+const path = require('path');
 
-module.exports = multer({
-    storage: multer.diskStorage({
-        destination: './upload/',
-        filename: function(req, file, returnCallback) {
-            returnCallback(null, file.originalname)
-        }
-    })
-})
+// Define the storage location for uploaded files
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/tmp'); // Use the /tmp directory
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+// Create the multer instance
+const upload = multer({ storage: storage });
+
+module.exports = upload;
